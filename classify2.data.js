73bf098bd99c0da643d175f335e1f7f51a0df5a8
BamboSun <33076058+BamboSun@users.noreply.github.com>
2018-06-23 10:01:35 +0800

    js

---
+++ classify2.data.js
@@ -0,0 +1,20 @@
+var inData = [{
+	value: '110000',
+	text: '收入',
+	children: [{
+		value: "110101",
+		text: "生活费"
+	}, {
+		value: "110102",
+		text: "奖学金"
+	}, {
+		value: "110103",
+		text: "理财收入"
+	}, {
+		value: "110103",
+		text: "兼职收入"
+	}, {
+		value: "110104",
+		text: "利息收入"
+	}]
+}]
